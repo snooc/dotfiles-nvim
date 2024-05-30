5a41196b889ac4aaaeebde2a69c28c690a95a4dc
Cody Coons <cody@codycoons.com>
2024-05-30 09:10:05 -0400

    Redo all

---
+++ lua/options.lua
@@ -0,0 +1,17 @@
+vim.opt.termguicolors = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.writebackup = false
+
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.wrap = false
+
+vim.opt.scrolloff = 5
+
+vim.g.mapleader = " "
